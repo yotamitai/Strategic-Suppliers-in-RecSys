a7753afacec2c3f9d7b1286e579924d2ce645328
yanaySoker <101624266+yanaySoker@users.noreply.github.com>
2023-03-02 09:51:05 +0200

    start heatmap

--- StratSupp/plotting.py
+++ StratSupp/plotting.py
@@ -1,4 +1,7 @@
 import matplotlib.pyplot as plt
+import seaborn
+
+
 def plot_supplier_over_time(df, subject):
     df.pivot('timestamp', 'supplier', subject).plot.line(
         title=f'Supplier {subject} over time ',
@@ -8,3 +11,7 @@ def plot_supplier_over_time(df, subject):
         )
     plt.show()
 
+
+def heatmap(values, measurement_name):
+
+    seaborn.heatmap(values)
